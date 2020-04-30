d0f64b6ed836bf0c927dc9e9aa895bb6e01c0651
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-04-30 21:11:53 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.join(__dirname, 'dist');
+const APP_DIR = path.join(__dirname, 'src');
+
+const config = {
+    entry: APP_DIR + '/app.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'app.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                // include: APP_DIR,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: 'file-loader'
+            }
+        ]
+    }
+}
+
+module.exports = config;
